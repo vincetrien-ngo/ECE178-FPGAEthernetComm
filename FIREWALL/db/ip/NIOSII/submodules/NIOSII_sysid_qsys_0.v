848013be2f06f6d2bb84ea62ad7833a9528299ff
tucker0410 <tucker.0410@gmail.com>
2020-04-23 12:58:59 -0700

    full compile success
    needs test in eclipse

--- FIREWALL/db/ip/NIOSII/submodules/NIOSII_sysid_qsys_0.v
+++ FIREWALL/db/ip/NIOSII/submodules/NIOSII_sysid_qsys_0.v
@@ -49,7 +49,7 @@ module NIOSII_sysid_qsys_0 (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1587664094 : 0;
+  assign readdata = address ? 1587671406 : 0;
 
 endmodule
 
